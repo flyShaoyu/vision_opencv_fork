59a5d207d8c219a05c6ffeab8b32a32ffeff8a2d
flyShaoyu <2733813241@qq.com>
2026-01-27 13:42:50 +0000

    将qr码部分移入src目录下，并在launch文件中添加对应launch的启动配置。

--- src/image_setup_launch/launch/image_launch.py
+++ src/image_setup_launch/launch/image_launch.py
@@ -6,7 +6,6 @@ from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
-    """Launch the image setup and depth camera nodes."""
     
     spear_vision_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -14,6 +13,16 @@ def generate_launch_description():
                 FindPackageShare('spear_vision'),
                 'launch',
                 'small_board_pose.launch.py',
+            ])
+        )
+    )
+
+    qr_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([
+                FindPackageShare('comm_ws'),
+                'launch',
+                'kfs_qr.launch.py',
             ])
         )
     )
@@ -27,40 +36,6 @@ def generate_launch_description():
             output='screen',
         ),
 
-        # R1机器人：二维码显示节点
-        # Node(
-        #     package='vision_opencv',
-        #     executable='qr_detect_node',
-        #     name='qr_display_r1',
-        #     output='screen',
-        #     parameters=[{'node_type': 'R1'}],
-        #     emulate_tty=True,
-        # ),
-        
-        # R2机器人：摄像头节点
-        Node(
-            package='vision_opencv',
-            executable='camera_node',
-            name='camera_r2',
-            output='screen',
-            parameters=[
-                {'camera_index': 11},
-                {'fps': 60},
-                {'brightness': 10.0},
-                {'contrast': 8.0},
-                {'exposure': 300.0}
-            ],
-            emulate_tty=True,
-        ),
-        
-        # R2机器人：二维码识别节点
-        Node(
-            package='vision_opencv',
-            executable='qr_detect_node',
-            name='qr_detect_r2',
-            output='screen',
-            parameters=[{'node_type': 'R2'}],
-            emulate_tty=True,
-        ),
         spear_vision_launch,
+        qr_launch,
     ])
